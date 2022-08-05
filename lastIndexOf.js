ac86a40e58c6ec7ef5fca553c24571464c7307bd
willianchu <willianchu@hotmail.com>
2022-08-05 08:27:30 -0700

    fixed with a while instead forloop

--- lastIndexOf.js
+++ lastIndexOf.js
@@ -1,14 +1,33 @@
 let lastIndexOf = function (array, value) {
   let output = 0;
-  for (let i = array.length - 1; i >= 0; i--) {
+  let i = array.length - 1;
+  while (i >= 0) {
     if (array[i] === value) {
       return i;
     }
-    output = i;
-  } return output;
+    i--;
+  }
+  output = -1;
+  return output;
 }
 console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
 console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
 console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
 console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
 console.log(lastIndexOf([], 3), "=?", -1);
+
+// student original code
+// let lastIndexOf = function (array, value) {
+//   let output = 0;
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     if (array[i] === value) {
+//       return i;
+//     }
+//     output = i;
+//   } return output;
+// }
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+// console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+// console.log(lastIndexOf([], 3), "=?", -1);
