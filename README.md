ac6f258f95a3e25258ef84d4da0dc02039b17620
willianchu <willianchu@hotmail.com>
2022-08-05 07:43:12 -0700

    set problem to debug

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # javascript_debug
-# javascript_debug
+
+Helping new students debug their javascript code.
+Not teaching a new or "correct" way to do things, but fixing their own code ideas.
+Just in extreme cases showing another way to do something.
+
---
+++ loopsLighthouse.js
@@ -0,0 +1,11 @@
+for (var x = 100; x <= 200;++x) {
+  if (x % 3 === 0){
+    console.log("Loopy");
+  } else if (x % 4 === 0) {
+    console.log("Lighthouse");
+  } else if (x % 3 === 0 && x % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else {
+  console.log(x);
+  }
+}
