31f2f8101c01f56108e4c92e32079c2c11bd375c
willianchu <willianchu@hotmail.com>
2022-08-10 21:24:02 -0700

    refactored

--- itsValley.js
+++ itsValley.js
@@ -20,7 +20,27 @@ function itsValleys(arr) {
   return true;
 }
 
+function itsValleys(arr) {
+  let isDescending = true;
+  let lastValue = arr[0];
+  
+  for(let i = 1; i < arr.length; i++) {
+    if(arr[i] < lastValue) {
+      // is descending
+      if(isDescending === false) {
+        return false;
+      }
+      lastValue = arr[i];
+    } else if(arr[i] > lastValue) {
+      // is climbing
+      isDescending = false;
+      lastValue = arr[i];
+    }
+  }
+  return true;
+}
 
-let valley = [9,8,7,6,5,5,4,3,2,1,2,1,2,3,3,4,5,6,7,8,9];
+
+let valley = [9,8,7,6,5,5,4,3,2,2,1,2,3,3,4,5,6,7,8,9];
 
 console.log(itsValleys(valley));
