05cfce05ab1a5f0e9a546de31f1a4ded0d59fd14
willianchu <willianchu@hotmail.com>
2022-08-10 20:38:22 -0700

    final version

--- SinglePeak.js
+++ SinglePeak.js
@@ -5,7 +5,7 @@ function singlePeak(arr) {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > lastValue) {
      // is climbing
-     if (!isClimbing) {
+     if (isClimbing === false) {
        return false;
      }
      lastValue = arr[i];
@@ -28,7 +28,8 @@ function singlePeak(arr) {
 
 
 
-let landscape = [1,2,3,4,5,6,7,8,9,9,9,8,7,7,6,5,4,3,2,1];
+// let singlePeak = [1,2,3,4,5,6,7,8,9,9,9,8,7,7,6,5,4,3,2,1];
+let multiple = [1,2,3,4,5,6,7,8,9,8,9,9,8,7,7,6,5,4,3,2,1];
 
-console.log(singlePeak(landscape));
+console.log(singlePeak(multiple));
 
