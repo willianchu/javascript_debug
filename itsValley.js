2c39b57cca99f024d1dd6723893c3a499302b8ef
willianchu <willianchu@hotmail.com>
2022-08-10 20:44:39 -0700

    valleyVersion

---
+++ itsValley.js
@@ -0,0 +1,26 @@
+function itsValleys(arr) {
+  let isDescending = true;
+  let lastValue = arr[0];
+  for(let i = 1; i < arr.length; i++) {
+    if(arr[i] < lastValue) {
+      // is descending
+      if(isDescending === false) {
+        return false;
+      }
+      lastValue = arr[i];
+    } else if(arr[i] > lastValue) {
+      // is climbing
+      isDescending = false;
+      lastValue = arr[i];
+    } else {
+      // is a plateau
+      continue;
+    }
+  }
+  return true;
+}
+
+
+let valley = [9,8,7,6,5,5,4,3,2,1,2,1,2,3,3,4,5,6,7,8,9];
+
+console.log(itsValleys(valley));
