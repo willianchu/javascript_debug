f20fd557a4f04d966a35cb8a4c8f1c7cbe3aff1b
willianchu <willianchu@hotmail.com>
2022-08-10 21:06:39 -0700

    refactor answer

--- SinglePeak.js
+++ SinglePeak.js
@@ -1,7 +1,30 @@
+// function singlePeak(arr) {
+//   let isClimbing = true;
+//   let lastValue = arr[0];
+  
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > lastValue) {
+//      // is climbing
+//      if (isClimbing === false) {
+//        return false;
+//      }
+//      lastValue = arr[i];
+//     } else if (arr[i] < lastValue) {
+//       // is descending
+//       isClimbing = false;
+//       lastValue = arr[i];
+//     } else {
+//       // is a plateau
+//       continue;
+//     }
+//   }
+//   return true;
+// }
+
+
 function singlePeak(arr) {
   let isClimbing = true;
   let lastValue = arr[0];
-  
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > lastValue) {
      // is climbing
@@ -13,10 +36,7 @@ function singlePeak(arr) {
       // is descending
       isClimbing = false;
       lastValue = arr[i];
-    } else {
-      // is a plateau
-      continue;
-    }
+    } 
   }
   return true;
 }
@@ -31,5 +51,5 @@ function singlePeak(arr) {
 // let singlePeak = [1,2,3,4,5,6,7,8,9,9,9,8,7,7,6,5,4,3,2,1];
 let multiple = [1,2,3,4,5,6,7,8,9,8,9,9,8,7,7,6,5,4,3,2,1];
 
-console.log(singlePeak(multiple));
+console.log(singlePeak(singlePeak));
 
