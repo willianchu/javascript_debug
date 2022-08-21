0fce1f6daf31ffc42d6927b79ee689b1a765fc36
willianchu <willianchu@hotmail.com>
2022-08-20 22:28:06 -0700

    console log variables

--- inBetween.js
+++ inBetween.js
@@ -2,17 +2,23 @@ function getTotalX(a, b) {
   let output = []
   // track int
    for(let i=1; i<=Math.max(...b); i++) {
+     console.log("this is external loop i",i)
     let mods = 0
     // track elements of a
     for(let j=0; j<a.length; j++) {
       mods += i % a[j]
+      console.log("mods",mods)
     }
     if(mods === 0) {
+      console.log("if mods ZERO")
       // track elems of b
       for(let k=0; k<b.length; k++) {
         mods += b[k] % i 
+        console.log("second mods",mods)
       }
+      console.log("if mods step II")
       if(mods === 0) {
+        console.log("push",i)
         output.push(i)
       } else continue
     } else continue
