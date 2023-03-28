1e21a07f52d796028b206fa923cccec9b0ac531d
willianchu <willianchu@hotmail.com>
2023-03-28 09:55:52 -0700

    inverting thousand

--- writeMoney.js
+++ writeMoney.js
@@ -2,39 +2,47 @@ function writeMoney(value) {
   const words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
                  'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
   const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const scales = ['', 'thousand', 'million', 'billion'];
   let dollars = Math.floor(value);
   let cents = Math.round((value - dollars) * 100);
+  let scaleIndex = 0;
   let result = '';
   if (dollars > 0) {
-    if (dollars >= 1000000) {
-      result += writeMoney(Math.floor(dollars / 1000000)) + ' million ';
-      dollars %= 1000000;
-    }
-    if (dollars >= 1000) {
-      result += writeMoney(Math.floor(dollars / 1000)) + ' thousand ';
-      dollars %= 1000;
-    }
-    if (dollars >= 100) {
-      result += words[Math.floor(dollars / 100)] + ' hundred ';
-      dollars %= 100;
-    }
-    if (dollars >= 20) {
-      result += tens[Math.floor(dollars / 10) - 2] + ' ';
-      dollars %= 10;
-    }
-    if (dollars > 0) {
-      result += words[dollars] + ' ';
+    while (dollars > 0) {
+      if (dollars >= 1000) {
+        const subDollars = dollars % 1000;
+        if (subDollars > 0) {
+          result = `${writeMoney(subDollars)} ${scales[scaleIndex]} ${result}`;
+        }
+        dollars = Math.floor(dollars / 1000);
+        scaleIndex++;
+      } else {
+        if (dollars >= 100) {
+          result += `${words[Math.floor(dollars / 100)]} hundred `;
+          dollars %= 100;
+        }
+        if (dollars >= 20) {
+          result += `${tens[Math.floor(dollars / 10) - 2]} `;
+          dollars %= 10;
+        }
+        if (dollars > 0) {
+          result += `${words[dollars]} `;
+        }
+        result += `${scales[scaleIndex]} `;
+        dollars = 0;
+        scaleIndex++;
+      }
     }
     result += 'dollars ';
   }
   if (cents > 0) {
     result += 'and ';
     if (cents >= 20) {
-      result += tens[Math.floor(cents / 10) - 2] + ' ';
+      result += `${tens[Math.floor(cents / 10) - 2]} `;
       cents %= 10;
     }
     if (cents > 0) {
-      result += words[cents] + ' ';
+      result += `${words[cents]} `;
     }
     result += 'cents';
   } else {
@@ -44,4 +52,4 @@ function writeMoney(value) {
 }
 
 
-console.log(writeMoney(50.60)); // Output: fifty dollars and sixty cents
+console.log(writeMoney(155.62)); 
