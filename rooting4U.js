c9a9d402e436b2cf120d471bead8c6d102fd5b39
willianchu <willianchu@hotmail.com>
2023-04-05 21:59:26 -0700

    extension error

--- rooting4U.js
+++ rooting4U.js
@@ -1,4 +1,5 @@
-import { expect } from 'chai'
+// Algorithm construction
+// const { expect } = require('chai');
 
 const judgeVegetable = function (vegetables, metric) {
   let highest = 0;
@@ -12,40 +13,68 @@ const judgeVegetable = function (vegetables, metric) {
   return winner;
 }
 
-let vegetables = [
-  {
-    submitter: 'Old Man Franklin',
-    redness: 10,
-    plumpness: 5
-  },
-  {
-    submitter: 'Sally Tomato-Grower',
-    redness: 2,
-    plumpness: 8
-  },
-  {
-    submitter: 'Hamid Hamidson',
-    redness: 4,
-    plumpness: 3
-  }
-]
-let metric = 'redness'
-let result = judgeVegetable(vegetables, metric)
-expect(result).to.equal('Old Man Franklin')
-vegetables = [
-  {
-    submitter: 'A',
-    d: 5,
-  },
-  {
-    submitter: 'B',
-    d: 10,
-  },
-  {
-    submitter: 'C',
-    d: 25,
-  }
-]
-metric = 'd'
-result = judgeVegetable(vegetables, metric)
-expect(result).to.equal('C')
+// let vegetables = [
+//   {
+//     submitter: 'Old Man Franklin',
+//     redness: 10,
+//     plumpness: 5
+//   },
+//   {
+//     submitter: 'Sally Tomato-Grower',
+//     redness: 2,
+//     plumpness: 8
+//   },
+//   {
+//     submitter: 'Hamid Hamidson',
+//     redness: 4,
+//     plumpness: 3
+//   }
+// ]
+// let metric = 'redness'
+// let result = judgeVegetable(vegetables, metric)
+// expect(result).to.equal('Old Man Franklin')
+// vegetables = [
+//   {
+//     submitter: 'A',
+//     d: 5,
+//   },
+//   {
+//     submitter: 'B',
+//     d: 10,
+//   },
+//   {
+//     submitter: 'C',
+//     d: 25,
+//   }
+// ]
+// metric = 'd'
+// result = judgeVegetable(vegetables, metric)
+// expect(result).to.equal('C')
+
+// Test
+
+// Import the Mocha library and the Chai assertion library
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+// Import the function to be tested
+// const judgeVegetable = require('./judgeVegetable');
+
+// Define the test suite
+describe('judgeVegetable', () => {
+  // Define the test case for redness
+  it('returns the submitter with the highest redness value', () => {
+    const vegetables = [      {        submitter: 'Old Man Franklin',        redness: 10,        plumpness: 5,      },      {        submitter: 'Sally Tomato-Grower',        redness: 2,        plumpness: 8,      },      {        submitter: 'Hamid Hamidson',        redness: 4,        plumpness: 3,      },    ];
+    const metric = 'redness';
+    const winner = judgeVegetable(vegetables, metric);
+    expect(winner).to.equal('Old Man Franklin');
+  });
+
+  // Define the test case for plumpness
+  it('returns the submitter with the highest plumpness value', () => {
+    const vegetables = [      {        submitter: 'Alice',        redness: 5,        plumpness: 10,      },      {        submitter: 'Bob',        redness: 8,        plumpness: 5,      },      {        submitter: 'Charlie',        redness: 2,        plumpness: 20,      },    ];
+    const metric = 'plumpness';
+    const winner = judgeVegetable(vegetables, metric);
+    expect(winner).to.equal('Charlie');
+  });
+});
